c07940aa18c831eb48654ea097e8fd198a7b1031
Nellbert Ethan Pineda <nellbert.pineda@student.ateneo.edu>
2024-11-25 17:27:08 +0800

    Add event database feature

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
